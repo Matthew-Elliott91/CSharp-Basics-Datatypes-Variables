31a04aaf37cd9976836ff2dccc6d3965d8dff620
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-19 16:52:57 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/// You need to define data types in typed languages
+/// declare a (string)
+
+string myFriendsName;
+myFriendsName = "Matthew";
+Console.WriteLine(myFriendsName);
